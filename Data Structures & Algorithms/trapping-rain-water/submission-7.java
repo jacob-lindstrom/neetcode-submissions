0758bab426ecb4f164a98567ab45d829f4ea41ac
jacob-lindstrom <jake.a.lindstrom@gmail.com>
2026-09-01 20:07:08 -0700

    Add: trapping-rain-water - submission-7

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-7.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int trap(int[] height) {
+        
+        if (height.length == 0){
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int result = 0;
+
+        while(left < right){
+            if (height[left] < height[right]){
+                left++;
+                leftMax = Math.max(leftMax, height[left]);
+                result += leftMax - height[left];
+            }
+            else {
+                right--;
+                rightMax = Math.max(rightMax, height[right]);
+                result += rightMax - height[right];
+            }
+
+
+        }
+
+        return result;
+
+    }
+}
