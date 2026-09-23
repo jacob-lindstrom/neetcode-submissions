d20c6fd609492da687fc8d880bc6fb7915f7ff6e
jacob-lindstrom <jake.a.lindstrom@gmail.com>
2026-09-23 15:41:52 -0700

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+        char[] word1 = s.toCharArray();
+        char[] word2 = t.toCharArray();
+
+        Arrays.sort(word1);
+        Arrays.sort(word2);
+
+        for (int i = 0; i < s.length(); i++){
+            if (word1[i] != word2[i]) return false;
+        }
+
+        return true;
+
+    }
+}
